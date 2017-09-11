fe6fb33e1af8a2a4edcb22a2a898073ad3a8795c
jymcheong <jym1976@gmail.com>
2017-09-11 17:10:35 +0800

    mod comment

--- test_msf.py
+++ test_msf.py
@@ -3,7 +3,7 @@ from msf_exploit_autocomplete import exploit
 from msf_post_autocomplete import post
 from msf_payload_autocomplete import payload
 # there may be illegal characters, especially within options class variables
-# the import will fail if any found
+# the import will fail there are illegal characters within class
 # print some stuff
 print(payload.windows_meterpreter_reverse_https.path)
 print(exploit.windows_winrm_winrm_script_exec.path)
