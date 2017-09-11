294b8935f70e88657b22ae30e78c03d245308324
jymcheong <jym1976@gmail.com>
2017-09-11 16:54:50 +0800

    shortened the code further

--- generate_msf_autocomplete.py
+++ generate_msf_autocomplete.py
@@ -14,23 +14,21 @@ def generate_codes(client, source_code, module_type):
         modules = client.modules.post
 
     for m in modules:
-        if not m.startswith('windows'): # 1600++ modules so let's limit it
-            continue
-        e = client.modules.use(module_type, m)        
-        source_code += "\n\tclass {0}(object):".format(m.replace('/', '_'))
-        source_code += '\n\t\t"""'
-        source_code += "\n\t\t{0}".format(e.description.replace('\\','/'))
-        source_code += '\n\t\t"""\n'        
-        source_code += "\n\t\tpath = '{0}'".format(m)
-        source_code += "\n\t\tclass options(object):"
-        for o in e.options:
-            key = o.replace("::", '_')
-            key = key.replace(' ', '_')
-            if o in e.required:                
-                source_code += "\n\t\t\t{0} = '{1}'".format('required_' + key, o)
-            else:
+        if m.startswith('windows'): # so let's limit it to windows for now
+            e = client.modules.use(module_type, m)        
+            source_code += "\n\tclass {0}(object):".format(m.replace('/', '_'))
+            source_code += '\n\t\t"""'
+            source_code += "\n\t\t{0}".format(e.description.replace('\\','/'))
+            source_code += '\n\t\t"""\n'        
+            source_code += "\n\t\tpath = '{0}'".format(m)
+            source_code += "\n\t\tclass options(object):"
+            for o in e.options:
+                key = o.replace("::", '_')
+                key = key.replace(' ', '_')
+                if o in e.required:                
+                    key = 'required_' + key
                 source_code += "\n\t\t\t{0} = '{1}'".format(key, o)        
-        source_code += "\n"
+            source_code += "\n"
     return source_code
 
 header = '""" This is generated autocomplete helper class for MSF """'
