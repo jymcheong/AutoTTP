92071ff4173a59239c63595e0a93fbe3417c8d51
jymcheong <jym1976@gmail.com>
2017-09-11 17:06:59 +0800

    remove \n

--- generate_msf_autocomplete.py
+++ generate_msf_autocomplete.py
@@ -34,11 +34,9 @@ header = '""" This is generated autocomplete helper class for MSF """'
 source_code = generate_codes(client, header, 'payload') 
 with open("msf_payload_autocomplete.py", "w") as text_file:
     text_file.write(source_code)
-    
 source_code = generate_codes(client, header, 'exploit') 
 with open("msf_exploit_autocomplete.py", "w") as text_file:
     text_file.write(source_code)
-
 source_code = generate_codes(client, header, 'post') 
 with open("msf_post_autocomplete.py", "w") as text_file:
     text_file.write(source_code)
