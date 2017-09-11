827f64bf5b6fa14ea103240b2912becd24455510
jymcheong <jym1976@gmail.com>
2017-09-11 16:46:54 +0800

    added comment for test script

--- test_msf.py
+++ test_msf.py
@@ -1,7 +1,10 @@
+" make sure all the modules can import/load"
 from msf_exploit_autocomplete import exploit
 from msf_post_autocomplete import post
 from msf_payload_autocomplete import payload
-
+# there may be illegal characters, especially within options class variables
+# the import will fail if any found
+# print some stuff
 print(payload.windows_meterpreter_reverse_https.path)
 print(exploit.windows_winrm_winrm_script_exec.path)
 print(post.windows_capture_keylog_recorder.path)
