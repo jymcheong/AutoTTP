8ece65ed668dcfd36db7f4ea228e06f22b01b216
jymcheong <jym1976@gmail.com>
2017-09-12 11:44:55 +0800

    add comment

--- bypassUAC_procedure_example.py
+++ bypassUAC_procedure_example.py
@@ -34,5 +34,5 @@ try:
 except Exception as e:
     exc_type, exc_obj, exc_tb = sys.exc_info()
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    print(exc_type, fname, exc_tb.tb_lineno)
+    print(exc_type, fname, exc_tb.tb_lineno) # provides exact line #
     print("Oops: " + str(e))
