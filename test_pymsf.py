5895c75e9cdafd14030cefaeb1dce753a2fe6bb7
jc <jym@jym.sg>
2017-09-28 16:25:12 +0800

    need to recap pymetasploit...

--- test_pymsf.py
+++ test_pymsf.py
@@ -1,10 +1,14 @@
+from time import sleep
 from c2_settings import MSF_SERVER, MSF_PWD
 from pymetasploit.msfrpc import MsfRpcClient
-
+from stage2.external_c2 import msf_wait_for_session
 
 client = MsfRpcClient(MSF_PWD, server=MSF_SERVER,ssl=False)
-sessions = client.sessions.list
-
-
+id = msf_wait_for_session.run(client, '172.30.1.83')
+shell = client.sessions.session(id)
+shell.write('sysinfo')
+sleep(5)
+r = shell.read()
+print(r)
 
 pass # for breakpoint
