fb211e474494191d63e97fcf1de16edbf1439541
jc <jym@jym.sg>
2017-10-19 16:33:35 +0800

    increase sleep interval

--- stage2/external_c2/empire_wait_for_agent.py
+++ stage2/external_c2/empire_wait_for_agent.py
@@ -16,12 +16,13 @@ def run(API, host_name, need_privilege=False, time_out_sec = 180):
     """
     time_out = time_out_sec
     agent_name = ""
+    step = 2
     while time_out > 0:
         agent_name = API.agent_get_name(host_name, need_privilege)        
         if len(agent_name) > 0:
             return API.agent_info(agent_name)['agents'][0]
-        time_out -= 1
-        sleep(1)
+        time_out -= step
+        sleep(step)
     raise ValueError('Wait for agent timeout')
 
 
