2300e17dc53163b8951a0cd71d85173acd5eb0d4
Jym Cheong <jym@jym.sg>
2017-10-19 15:51:44 +0800

    Create c2_settings.py

---
+++ c2_settings.py
@@ -0,0 +1,8 @@
+EMPIRE_SERVER='empirec2'
+EMPIRE_USER='empireadmin'
+EMPIRE_PWD='Password123'
+# ./empire --rest --username empireadmin --password Password123
+
+MSF_SERVER='msfc2'
+MSF_PWD='test'
+# msfrpcd -P test -S -f
