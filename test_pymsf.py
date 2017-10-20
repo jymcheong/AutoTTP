d4fd95582b08bfb2bd1aade6905a40b6da7b111c
jc <jym@jym.sg>
2017-10-20 10:48:58 +0800

    wait for session without IP address

--- test_pymsf.py
+++ test_pymsf.py
@@ -4,7 +4,7 @@ from pymetasploit.msfrpc import MsfRpcClient
 from stage2.external_c2 import msf_wait_for_session, msf_get_timestamp
 
 client = MsfRpcClient(MSF_PWD, server=MSF_SERVER,ssl=False)
-id = msf_wait_for_session.run(client, '192.168.181.191', meterpreter=False)
+id = msf_wait_for_session.run(client)
 shell = client.sessions.session(id)
 if(shell is None):
     print("time out, no shell :(")
