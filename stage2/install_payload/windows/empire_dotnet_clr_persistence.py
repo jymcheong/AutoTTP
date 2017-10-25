c206664b538f680c003b7a8a8670aaff475387ba
jc <jym@jym.sg>
2017-10-25 10:12:20 +0800

    added @3gstudent 's POC link for AppDomainManager persistence

--- stage2/install_payload/windows/empire_dotnet_clr_persistence.py
+++ stage2/install_payload/windows/empire_dotnet_clr_persistence.py
@@ -1,5 +1,7 @@
 """
 TODO
 PoC: https://github.com/3gstudent/CLR-Injection
+https://3gstudent.github.io/3gstudent.github.io/Use-CLR-to-maintain-persistence/
+
 Details: http://subt0x10.blogspot.sg/2017/05/subvert-clr-process-listing-with-net.html
 """
